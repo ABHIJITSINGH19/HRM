41617ca377451aac7684c4941dde086da658a3c2
ABHIJITSINGH19 <132670625+ABHIJITSINGH19@users.noreply.github.com>
2025-07-21 17:47:52 +0530

    Update main.jsx

--- PSQUARE/project/src/main.jsx
+++ PSQUARE/project/src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { setLogoutTimer } from "./redux/slice/authSlice";
+import { setLogoutTimer } from "./redux/slice/authSlice.js";
+
+setLogoutTimer(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-    {setLogoutTimer(store.dispatch)}
   </React.StrictMode>
 );
