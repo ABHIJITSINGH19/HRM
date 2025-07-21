586cefde26aa62f7b5065963f081ad67c6025589
ABHIJITSINGH19 <132670625+ABHIJITSINGH19@users.noreply.github.com>
2025-07-21 17:46:51 +0530

    Update ProtectedRoute.jsx

--- PSQUARE/project/src/routes/ProtectedRoute.jsx
+++ PSQUARE/project/src/routes/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ProtectedRoute = () => {
-  const token = sessionStorage.getItem("token");
+  const { token } = useSelector((state) => state.auth);
   if (!token) {
     return <Navigate to="/" />;
   }
